9c92e40a04e885a87bb087696a190fe468023d81
Rafael <bechstedt@hotmail.com>
2023-06-28 16:51:52 -0300

    Req 8

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -28,5 +28,29 @@ def exists_word(word, instance: Queue):
     return result
 
 
-def search_by_word(word, instance):
-    ...
+def search_by_word(word, instance: Queue):
+    result = []
+
+    for i in range(len(instance)):
+        file = instance.search(i)
+
+        occurrence_list = []
+
+        line_index = 1
+        for line in file["linhas_do_arquivo"]:
+            if word.lower() in line.lower():
+                occurrence_list.append({
+                    "linha": line_index,
+                    "conteudo": line
+                })
+
+            line_index += 1
+
+        if occurrence_list:
+            result.append({
+                "palavra": word,
+                "arquivo": file["nome_do_arquivo"],
+                "ocorrencias": occurrence_list
+            })
+
+    return result
